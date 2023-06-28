248c85f7e8d0f6d7a2d0e85684f3264e19528db8
Blubywaff <45488563+Blubywaff@users.noreply.github.com>
2023-06-27 20:41:02 -0500

    add tags to file upload

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"github.com/google/uuid"
+	"context"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
-	// "github.com/neo4j/neo4j-go-driver/v5/neo4j"
+
+	"github.com/google/uuid"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 var templates *template.Template
+
+var session neo4j.SessionWithContext
+
+var ctx context.Context
 
 func landingPage(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(200)
@@ -75,18 +82,53 @@ func uploadPage(res http.ResponseWriter, req *http.Request) {
 		do_del = true
 		return
 	}
+    tagstr := req.FormValue("tags")
+    tags := strings.Split(tagstr, ",")
+    _, err = session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+        _, err := tx.Run(ctx, `
+        CREATE (a:File {id: $fid})
+        FOREACH (tag in $tags |
+            MERGE (t:Tag {name: tag})
+            CREATE (t)-[:describes]->(a)
+        )`, map[string]any{"fid": id, "tags": tags})
+        if err != nil {
+            return nil, err
+        }
+        return nil, nil
+    })
+    if err != nil {
+        log.Fatal(err)
+        log.Println("Database failed for file upload");
+        return;
+    }
 }
 
 func main() {
+    // Initialize context
+    ctx = context.Background()
+
 	// Load Templates
 	templates = template.Must(template.ParseGlob("./pages/*.html"))
 
+    // Load database connection
+    driver, err := neo4j.NewDriverWithContext("neo4j://localhost:7687", neo4j.NoAuth())
+    if err != nil {
+        log.Fatal("cannot connect to database")
+        return;
+    }
+    defer driver.Close(ctx)
+
+    session = driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+    defer session.Close(ctx)
+
 	statfs := http.FileServer(http.Dir("./dist"))
 	webfs := http.FileServer(http.Dir("./pages"))
+    filefs := http.FileServer(http.Dir("./files"))
 
 	http.HandleFunc("/", landingPage)
 	http.Handle("/public/", http.StripPrefix("/public/", statfs))
 	http.Handle("/site/", http.StripPrefix("/site/", webfs))
+	http.Handle("/files/", http.StripPrefix("/files/", filefs))
 	http.HandleFunc("/site/upload", uploadPage)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
